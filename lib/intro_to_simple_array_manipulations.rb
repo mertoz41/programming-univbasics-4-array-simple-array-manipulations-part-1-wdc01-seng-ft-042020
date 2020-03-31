25d5e72ade11191e2a71b5436ecbd7970a7db8c4
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-03-31 01:27:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,3 +15,15 @@ end
 def pop_with_args(array)
   array.pop(2)
 end 
+def using_shift(array)
+  array.shift
+end 
+
+def using_shift
+  array.shift(-1)
+end 
+
+def shift_with_args(array)
+  array.shift(2)
+end 
+
